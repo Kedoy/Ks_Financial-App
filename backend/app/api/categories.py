6e2b3c4466d34c09ccb9ac63e2a7a513932ca171
Kedoy <avekedoy@yandex.ru>
2026-01-16 23:37:45 +0300

    android done

--- backend/app/api/categories.py
+++ backend/app/api/categories.py
@@ -1,6 +1,4 @@
-# Добавь в начало файла
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import APIRouter, Depends, HTTPException, status
 
-# Определи oauth2_scheme локально
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
